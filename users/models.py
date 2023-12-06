e8e8468739a9af1f9749552b89e990780a204d1f
Ajit <ajitstha16@gmail.com>
2023-12-06 17:18:32 +0545

    created Profile

--- users/models.py
+++ users/models.py
@@ -1,2 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
+class Profile(models.Model):
+    is_recruiter = models.BooleanField(default=False)
+    is_recruitee = models.BooleanField(default=True)
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True, null=True)
+    cv = models.FileField(upload_to="cv/")
+    resume = models.FileField(upload_to="resume/")
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance,created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
